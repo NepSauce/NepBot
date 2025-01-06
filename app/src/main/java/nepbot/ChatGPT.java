64d2248b9ad3776face055e576c7d7ec995dced1
NepSauce <greasyandmoist@gmail.com>
2025-01-06 18:42:41 -0400

    Added Imports in ChatGPT.java

--- app/src/main/java/nepbot/ChatGPT.java
+++ app/src/main/java/nepbot/ChatGPT.java
@@ -1,5 +1,13 @@
 package nepbot;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ChatGPT {
-    
+    String apiKey = "";
 }
