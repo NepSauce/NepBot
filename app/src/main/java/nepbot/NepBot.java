f2bc2379f3ae7160ff89f8e3a768ae29c6d8ecdb
NepSauce <greasyandmoist@gmail.com>
2025-01-05 20:15:55 -0400

    Added Base Files

---
+++ app/src/main/java/nepbot/NepBot.java
@@ -0,0 +1,41 @@
+package nepbot;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class NepBot{
+    public static void main(String[] args)
+    throws LoginException{
+        String token = "0";
+        JDA jda = JDABuilder.createDefault(token).build();
+        
+    }
+
+    public static String fileReader(String fileName){
+        String token = "";
+
+        try{
+            File newFile = new File(fileName);
+            Scanner newReader = new Scanner(newFile);
+            
+            if (newReader.hasNextLine()){
+                token = newReader.nextLine();
+            }
+            else{
+                System.out.println("File Contents Are Empty"); 
+            }
+            
+        } 
+        catch (FileNotFoundException e){
+            System.out.println("File Was Not Found");
+        }
+
+        return token;
+    }
+}
