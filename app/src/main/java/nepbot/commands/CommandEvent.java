f365412b68754e06fe6c37b5caa1de33caa57fd2
Zawad Atif <greasyandmoist@gmail.com>
2025-01-10 17:08:41 -0400

    Added Join and Leave Commands

--- app/src/main/java/nepbot/commands/CommandEvent.java
+++ app/src/main/java/nepbot/commands/CommandEvent.java
@@ -1,21 +1,30 @@
 package nepbot.commands;
 
+import java.lang.reflect.Member;
+
 import org.jetbrains.annotations.NotNull;
 
 import nepbot.Event;
 import nepbot.commands.dad.DadCommand;
 import nepbot.commands.dad.HelpCommand;
+import nepbot.commands.music.JoinCommand;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 
 
 public class CommandEvent extends ListenerAdapter{
     public static String[] splitMessage;
     static Boolean dadBotMode = true;
     
-    @SuppressWarnings("null")
+    @SuppressWarnings({ "null", "unused" })
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event){
+        Guild guild = event.getGuild();
+        User member = event.getAuthor();
+
         if (event.getAuthor().isBot()){
             return;
         }
@@ -23,12 +32,30 @@ public class CommandEvent extends ListenerAdapter{
         splitMessage = Event.getSplitMessage();
         Boolean containsNep = splitMessage[0].contains("!nep");
 
-        if (containsNep && splitMessage[1].equalsIgnoreCase("dadMode")){
-            DadCommand.onDadCommandReceived(event);
+        if (containsNep){
+            if (splitMessage[1].equalsIgnoreCase("dadMode")){
+                DadCommand.onDadCommandReceived(event);
+            }
+            else if (splitMessage[1].equalsIgnoreCase("help")){
+                HelpCommand.onHelpCommandReceived(event);
+            }
+            else if (splitMessage[1].equalsIgnoreCase("grab")){
+                VoiceChannel channel = event.getMember().getVoiceState().getChannel().asVoiceChannel();
+    
+                if(channel != null && member != null){
+                    JoinCommand.joinChannel(channel, guild);
+                    event.getChannel().sendMessage("-Joined Current Voice Channel").queue();
+                }
+                else{
+                    event.getChannel().sendMessage("-Join a Voice Channel First.").queue();;
+                }
+            }
+            else if (splitMessage[1].equalsIgnoreCase("yeet")){
+                JoinCommand.leaveChannel(guild);
+                event.getChannel().sendMessage("-Disconnected From Voice Channel").queue();
+            }
         }
-        if (containsNep && splitMessage[1].equalsIgnoreCase("help")){
-            HelpCommand.onHelpCommandReceived(event);
-        }
+
     }
     
     public static Boolean getDadBotMode(){
--- app/src/main/java/nepbot/commands/music/JoinCommand.java
+++ app/src/main/java/nepbot/commands/music/JoinCommand.java
@@ -1,5 +1,15 @@
 package nepbot.commands.music;
 
-public class JoinCommand {
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+
+public class JoinCommand{
+    public static void joinChannel(VoiceChannel channel, Guild guild){
+        guild.getAudioManager().openAudioConnection(channel);
+    }
+
+    public static void leaveChannel(Guild guild){
+        guild.getAudioManager().closeAudioConnection();
+    }
 
 }
